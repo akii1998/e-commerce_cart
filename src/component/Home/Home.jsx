597cf07dcd59196ab1bb9a212f89d5a31b4eb1a2
akii1998 <110331686+akii1998@users.noreply.github.com>
2023-01-22 00:47:07 +0530

    Home Component

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>
-        
+    <div className="hero">
+      <div class="card text-white border-0">
+        <img
+          src="/assets/bg.webp"
+          class="card-img"
+          alt="BackGround"
+          height={"550px"}
+        />
+        <div class="card-img-overlay d-flex flex-column justify-content-center ">
+          <div className="container">
+            <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
+            <p class="card-text lead fs-2">
+              CHECK OUT ALL THE TENDS 
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
