b8339b013a921d4d9fdef257cb75ddb0ff8624cc
akii1998 <110331686+akii1998@users.noreply.github.com>
2023-01-22 01:41:29 +0530

    add loading pkg , latest product ,filter functionality

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,24 +1,26 @@
 import React from "react";
+import Product from "../Product/Product";
 
 const Home = () => {
   return (
     <div className="hero">
-      <div class="card text-white border-0">
+      <div className="card text-white border-0">
         <img
           src="/assets/bg.webp"
-          class="card-img"
+          className="card-img"
           alt="BackGround"
           height={"550px"}
         />
-        <div class="card-img-overlay d-flex flex-column justify-content-center ">
+        <div className="card-img-overlay d-flex flex-column justify-content-center ">
           <div className="container">
-            <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
-            <p class="card-text lead fs-2">
+            <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
+            <p className="card-text lead fs-2">
               CHECK OUT ALL THE TENDS 
             </p>
           </div>
         </div>
       </div>
+      <Product/>
     </div>
   );
 };
