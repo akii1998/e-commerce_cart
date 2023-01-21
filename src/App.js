fd5eac92b219588ae847909ae9b6ea66f25780f1
akii1998 <110331686+akii1998@users.noreply.github.com>
2023-01-22 00:31:14 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Home from './component/Home/Home';
 import Navbar from './component/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <Home/>
     </div>
   );
 }
