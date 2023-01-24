00133c16cea9643b9af82096660d35799c988e12
akii1998 <110331686+akii1998@users.noreply.github.com>
2023-01-24 09:36:53 +0530

    folder change

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import Login from "../button/Login";
-import SignUp from "../button/SignUp";
-import CartBtn from "../button/CartBtn";
+import Login from "../CustomButton/Login";
+import SignUp from "../CustomButton/SignUp";
+import CartBtn from "../CustomButton/CartBtn";
 
 const Header = () => {
   return (
